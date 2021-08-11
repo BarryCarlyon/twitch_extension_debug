27b4bde7ab384a0f4c535eef6ac189d678868fee
Barry Carlyon | Kora <barry@barrycarlyon.co.uk>
2021-08-11 15:29:37 +0100

    Fix a typo and move onFollow resp up

--- extension/panel/actions.js
+++ extension/panel/actions.js
@@ -13,23 +13,23 @@ actions_page.append(follow_button);
         window.Twitch.ext.actions.followChannel(channel_name);
     });
 
+let onfollow = document.createElement('div');
+actions_page.append(onfollow);
+
+window.Twitch.ext.actions.onFollow((didFollow, channel_name)=> {
+    master_log('Read actions.onFollow');
+    iterateObject(onfollow, {
+        didFollow,
+        channel_name
+    });
+});
+
 let minimise_button = document.createElement('button');
 actions_page.append(minimise_button);
     minimise_button.textContent = 'window.Twitch.ext.actions.minimize';
     minimise_button.addEventListener('click', (e) => {
         window.Twitch.ext.actions.minimize();
     });
-
-let onfollow = document.createElement('div');
-actions_page.append(onfollow);
-
-window.Twitch.ext.actions.onFollow((didFollow, channel_name)=> {
-    master_log('Read actions.onFollow');
-    iterateObject(onFollow, {
-        didFollow,
-        channel_name
-    });
-});
 
 let requestID_button = document.createElement('button');
 actions_page.append(requestID_button);
