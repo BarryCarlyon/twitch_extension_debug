aed5377b7b7a308199297b9b2e53369ca7312deb
Barry Carlyon | Robyn <barry@barrycarlyon.co.uk>
2022-05-18 17:12:12 +0100

    lets add extension CSP for local test And a blank env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
 
@@ -8,10 +10,29 @@ app.listen(8050, function () {
     console.log('booted express on 8050');
 });
 
+const twitchextensioncsp = require('twitchextensioncsp');
+app.use(twitchextensioncsp({
+    clientID: process.env.CLIENT_ID,
+    reportUri: process.env.REPORTURI
+}));
+
 app.use(function(req, res, next) {
     console.log("received from "+req.get("X-Forwarded-For")+" : "+req.method+" "+req.originalUrl);
     next();
 });
+
+/*
+This will capture any CSP Report and dump log it to console
+*/
+app.post('/csp/', express.json({
+    type: 'application/csp-report'
+}), (req,res) => {
+    res.send('Ok');
+
+    console.log(req.body);
+});
+
+// static route
 app.use('/extension/', express.static(path.join(
     __dirname,
     '..',
